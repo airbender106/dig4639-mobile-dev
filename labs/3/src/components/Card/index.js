9836fff1654c3d7f60c0b928a0a7f0b6aad19320
airbender106 <sdharmakirthi@Knights.ucf.edu>
2020-02-23 15:01:57 -0500

    3

---
+++ labs/3/src/components/Card/index.js
@@ -0,0 +1,20 @@
+Learn more or give us feedback
+import React from 'react';
+import './index.css';
+
+class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.props = props;
+        this.content = this.props.content;
+    }
+    render() {
+        return(
+            <div className="card">
+                {this.content}
+            </div>
+        )
+    }
+}
+
+export default Card;
